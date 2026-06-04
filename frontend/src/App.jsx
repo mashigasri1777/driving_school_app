485ab64ac1cf6f7cbd5d22a2aa310c752f886cda
mashigasri1777 <mashigasri1777@gmail.com>
2026-06-05 00:51:47 +0530

    Remove Fees and Schedule routes from App
    Removed Fees and Schedule routes from the App component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
 import Students from "./pages/Students";
-import Fees from "./pages/Fees";
-import Schedule from "./pages/Schedule";
 
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
@@ -11,20 +9,24 @@ import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <BrowserRouter>
+
       <Navbar />
 
       <div className="container">
+
         <Sidebar />
 
         <div className="content">
+
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/students" element={<Students />} />
-            <Route path="/fees" element={<Fees />} />
-            <Route path="/schedule" element={<Schedule />} />
           </Routes>
+
         </div>
+
       </div>
+
     </BrowserRouter>
   );
 }
