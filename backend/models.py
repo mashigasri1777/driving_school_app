24b5edc84a1a118d62ba329bc9259bd4309b7655
mashigasri1777 <mashigasri1777@gmail.com>
2026-06-04 22:27:49 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(20))
+    email = db.Column(db.String(100))
+    address = db.Column(db.String(200))
+
+    course = db.Column(db.String(100))
+    fee = db.Column(db.Float, default=0)
+    paid = db.Column(db.Float, default=0)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "phone": self.phone,
+            "email": self.email,
+            "address": self.address,
+            "course": self.course,
+            "fee": self.fee,
+            "paid": self.paid
+        }
