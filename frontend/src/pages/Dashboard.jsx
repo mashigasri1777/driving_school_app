45f1dc1d142c1ae8f953a97f40df137cd7631d8d
mashigasri1777 <mashigasri1777@gmail.com>
2026-06-04 22:38:11 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,1 +1,16 @@
+function Dashboard() {
+  return (
+    <div>
+      <div className="card">
+        <h2>Dashboard</h2>
 
+        <p>
+          Welcome to Driving School
+          Management System
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
