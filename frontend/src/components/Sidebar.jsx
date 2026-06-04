7a4fd0f73284ad387c877e90a097033e0c9359c5
mashigasri1777 <mashigasri1777@gmail.com>
2026-06-04 22:36:31 +0530

    Add Sidebar component with navigation links

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,1 +1,43 @@
+import { Link } from "react-router-dom";
 
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h3>Menu</h3>
+
+      <br />
+
+      <p>
+        <Link to="/" style={{ color: "white" }}>
+          Dashboard
+        </Link>
+      </p>
+
+      <br />
+
+      <p>
+        <Link to="/students" style={{ color: "white" }}>
+          Students
+        </Link>
+      </p>
+
+      <br />
+
+      <p>
+        <Link to="/fees" style={{ color: "white" }}>
+          Fees
+        </Link>
+      </p>
+
+      <br />
+
+      <p>
+        <Link to="/schedule" style={{ color: "white" }}>
+          Schedule
+        </Link>
+      </p>
+    </div>
+  );
+}
+
+export default Sidebar;
