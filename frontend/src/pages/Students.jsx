6be62add7e3468153fbc69e636863b629cad4d77
mashigasri1777 <mashigasri1777@gmail.com>
2026-06-04 22:38:44 +0530

    Update Students.jsx

--- frontend/src/pages/Students.jsx
+++ frontend/src/pages/Students.jsx
@@ -1,1 +1,36 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+import StudentForm from "../components/StudentForm";
+import StudentTable from "../components/StudentTable";
+
+function Students() {
+  const [students, setStudents] = useState([]);
+
+  const loadStudents = async () => {
+    const res = await axios.get(
+      "http://127.0.0.1:5000/api/students"
+    );
+
+    setStudents(res.data);
+  };
+
+  useEffect(() => {
+    loadStudents();
+  }, []);
+
+  return (
+    <>
+      <StudentForm
+        refreshStudents={loadStudents}
+      />
+
+      <StudentTable
+        students={students}
+        refreshStudents={loadStudents}
+      />
+    </>
+  );
+}
+
+export default Students;
