3f011cf777fa0f19d2ab435dff5320f5c02cdf2c
mashigasri1777 <mashigasri1777@gmail.com>
2026-06-04 22:28:36 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,28 @@
+from flask import Flask
+from flask_cors import CORS
 
+from models import db
+from routes import api
+
+app = Flask(__name__)
+
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+app.register_blueprint(api, url_prefix="/api")
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def home():
+    return {
+        "message": "Driving School API Running"
+    }
+
+if __name__ == "__main__":
+    app.run(debug=True)
