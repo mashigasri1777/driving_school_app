e024f00dfbe35ce148e00ff7feae4790855002b0
Mashiga Sri <mashigasri1777@gmail.com>
2026-06-13 22:26:39 +0530

    Update App.jsx with proper imports for About and LLRApplication pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Dashboard from "./pages/Dashboard";
 import Students from "./pages/Students";
 import About from "./pages/About";
 import LLRApplication from "./pages/LLRApplication";
+
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
+
       <Navbar />
+
       <div className="container">
+
         <Sidebar />
+
         <div className="content">
+
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/students" element={<Students />} />
             <Route path="/about" element={<About />} />
             <Route path="/llr-application" element={<LLRApplication />} />
           </Routes>
+
         </div>
+
       </div>
+
     </BrowserRouter>
   );
 }
